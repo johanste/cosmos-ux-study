f6126abfa5d48b1fd77f58199791836e05e80e2c
Johan Stenberg <johan.stenberg@microsoft.com>
2018-12-03 11:30:40 -0800

    Made steps into functions

--- program.py
+++ program.py
@@ -12,14 +12,16 @@ TITANIC_DATA_FILE = path.expanduser('~/usability/data/titanic.json')
 # 3) create a new container named 'titanic' in the database 'uxstudy'
 
 # 4) Upload each of the items in the titanic dataset into the titanic container
+def upload_data():
+    """ Upload (insert) all the data into the Azure Cosmos SQL DB Container
+    """
+    import json
+    with open(TITANIC_DATA_FILE, 'r') as f:
+        data = json.load(f)
 
-import json
-with open(TITANIC_DATA_FILE, 'r') as f:
-    data = json.load(f)
-
-for item in data:
-    # Ensure that each item is in the collection
-    pass
+    for item in data:
+        # Ensure that each item is in the collection
+        pass
 
 # Interlude: Go to the portal and look at the uploaded data...
 
@@ -30,16 +32,32 @@ for item in data:
 #
 #       SELECT * FROM passenger WHERE passenger.age = ''
 #
-
+def get_passengers_with_no_age():
+    """ Query for all passengers where the age is missing (age is set to an empty string)
+    """
+    pass
 
 # 6) Delete all items that do not have a registered age (returned in step #5) from the container
+def delete_passengers_with_no_age():
+    """ Cleans up the data in the container by deleting all passengers that have no
+    known age (age set to an empty string)
+    """
+    pass
 
 # 7) Find the average age for passengers that survived from first class
 #
 # You can use the following parameterized query 
 #       SELECT * from passenger WHERE passenger.pclass=@class AND passenger.survived=1
 #
-
+def compute_average_age(passenger_class=1):
+    """ Query for and compute the average age for each of the surviving passengers in the given 
+    class
+    """
+    pass
 
 # 8) Update/add a boolean attribute 'younger' to each item indicating if the person was younger or older than the average survivor for first class
-
+def augment_data():
+    """ Update each passenger with a new boolean property 'younger' indicating if the 
+    passenger was younger or older than the average age of first class surviving passengers
+    """
+    pass
